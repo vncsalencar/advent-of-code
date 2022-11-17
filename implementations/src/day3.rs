82bd1ca3c543b2167d19bab36c8f92a49151c865
Vinícius Borges Alencar <viniciusale4@gmail.com>
2022-11-17 20:41:22 -0300

    add day 3 part 2 solution

--- implementations/src/day3.rs
+++ implementations/src/day3.rs
@@ -22,8 +22,37 @@ fn part1() -> usize {
     visited.len()
 }
 
-fn part2() {
+fn part2() -> usize {
     let input = super::get_input("day3");
+
+    let mut santa_directions = (0, 0);
+    let mut robot_directions = (0, 0);
+    let mut visited = HashSet::<String>::new();
+
+    let mut count = 0;
+
+    input.trim().chars().for_each(|movement| {
+        let mut directions = if count % 2 == 0 {
+            &mut santa_directions
+        } else {
+            &mut robot_directions
+        };
+
+        let key = format!("{}:{}", directions.0, directions.1);
+        visited.insert(key);
+
+        match movement {
+            '<' => (*directions).0 -= 1,
+            'v' => (*directions).1 -= 1,
+            '^' => (*directions).1 += 1,
+            '>' => (*directions).0 += 1,
+            _ => unreachable!("This should not be called"),
+        };
+
+        count += 1;
+    });
+
+    visited.len()
 }
 
 #[cfg(test)]
@@ -38,5 +67,6 @@ mod perfectly_spherical_houses_in_a_vacuum {
     #[test]
     fn part2() {
         let answer = super::part2();
+        assert!(answer == 2631);
     }
 }
